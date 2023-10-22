7184ad467ef5510bcb7b2f135cf64c5c1bcc6b57
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-10-22 23:42:02 +0600

    latest

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const app = express;
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) =>{
-    
+    res.send("Users Management server is running")
+})
+app.listen(port,() =>{
+    console.log(`Server is running on port: ${port}`)
 })
