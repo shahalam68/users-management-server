d1d5f02a49445df391406bb04c08033599a76f42
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-10-23 13:04:34 +0600

    fake data

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users =[
+    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
+    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
+    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
+    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
+    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
+]
+
 app.get('/', (req, res) =>{
     res.send("Users Management server is running")
 })
