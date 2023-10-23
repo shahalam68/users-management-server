bf2971fdb2c84d43081a3dc1b056aebd8a5d414d
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-10-23 17:26:47 +0600

    fake data

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const port = process.env.PORT || 5000;
 
 const users =[
     {id:1, name: 'Shabana', email:' shabana@gmail.com'},
-    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
-    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
-    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
-    {id:1, name: 'Shabana', email:' shabana@gmail.com'},
+    {id:2, name: 'Shabnoor', email:' Shabnoor@gmail.com'},
+    {id:3, name: 'Shabila', email:' Shabila@gmail.com'},
 ]
 
 app.get('/', (req, res) =>{
-    res.send("Users Management server is running")
+    res.send(users)
 })
 app.listen(port,() =>{
     console.log(`Server is running on port: ${port}`)
