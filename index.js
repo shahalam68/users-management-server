1f8ba95b20596e8602b9158c35412647568397a5
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-10-24 10:34:09 +0600

    fake data

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+
+
 const users =[
     {id:1, name: 'Shabana', email:' shabana@gmail.com'},
     {id:2, name: 'Shabnoor', email:' Shabnoor@gmail.com'},
