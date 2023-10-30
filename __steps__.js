ed71eb506f050c27fd6febdc1848fdd5d972e119
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-10-30 23:35:30 +0600

    first commit

--- __steps__.js
+++ __steps__.js
@@ -4,7 +4,7 @@
  * 3. set fatch method inside the fetch opstions(second parameter)
  * 4.options will have three things:method
  * 5. Options will have headers:'content-type': 'application/json'
- * 6. body : Don't forget to send data by JSON stringify
+ * 6. body : Don't forget to send data by JSON stringif
  * 7. On the server side don't forgate to use express.json()
  * 
  */
